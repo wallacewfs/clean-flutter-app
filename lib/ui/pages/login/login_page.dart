7ce3487674d817b03c6347334f1b7444431589fe
wallacewfs <wallacef@gmail.com>
2024-02-05 19:09:26 -0300

    refactor: move error message  to its own file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -33,12 +33,7 @@ class _LoginPageState extends State<LoginPage> {
         widget.presenter.mainErrorStream.listen((error) {
           // ignore: unnecessary_null_comparison
           if (error != null) {
-           ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              backgroundColor: Colors.red[900],
-              content: Text(error,textAlign: TextAlign.center, )
-              )
-           );   
+           showErrorMessage(context, error);
           }
         });
 
