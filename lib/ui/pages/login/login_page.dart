91d2e4df09cd792b0f73f9d128993f2c98f2e255
wallacewfs <wallacef@gmail.com>
2024-02-06 20:07:26 -0300

    refactor: move Login Button  to its own file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -56,14 +56,7 @@ class _LoginPageState extends State<LoginPage> {
                           padding: const EdgeInsets.only(top: 8, bottom: 32),
                           child: PasswordInput(),
                         ),
-                        StreamBuilder<bool>(
-                            stream: widget.presenter.isFormValidStream,
-                            builder: (context, snapshot) {
-                              return ElevatedButton(
-                                onPressed: snapshot.data == true ? widget.presenter.auth : null,
-                                child: Text('Entrar'.toUpperCase()),
-                              );
-                            }),
+                        LoginButton(),
                         TextButton.icon(
                           icon: const Icon(Icons.person),
                           onPressed: () {},
@@ -79,3 +72,4 @@ class _LoginPageState extends State<LoginPage> {
     ));
   }
 }
+
