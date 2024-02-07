6b6984037900b2cea2cd5db6e1ce6d0547bdd0c4
wallacewfs <wallacef@gmail.com>
2024-02-07 12:11:50 -0300

    test: ensure LoginPresenter emits error if email is invalid

--- requirements/login/login_presenter.md
+++ requirements/login/login_presenter.md
@@ -3,7 +3,7 @@
     > ## Regras
 
      ✅ Chamar Validation ao alterar o email
-     Notificar o emailErrorStream com o mesmo erro do Validation, caso retorne erro
+     ✅ Notificar o emailErrorStream com o mesmo erro do Validation, caso retorne erro
      Notificar o emailErrorStream com null, caso o Validation não retorne erro
      Não notificar o emailErrorStream se o valor for igual ao último
      Notificar o isFormValidStream após alterar o email
--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
@@ -8,13 +10,23 @@ abstract class Validation {
   String validate({required String field , required String value});
 }
 
+class LoginState {
+  late String emailError;
+}
+
 class StreamLoginPresenter {
   final Validation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+
+  final _state = LoginState();
+
+  Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError);
 
   StreamLoginPresenter({required this.validation});
 
   void validateEmail(String email) {
-    validation.validate(field: 'email', value: email);
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
   }
 }
 
@@ -34,4 +46,14 @@ void main() {
 
     verify(() => validation.validate(field: 'email', value: email)).called(1);
   });
+
+  test('should emit email error if validaton fails', () {
+    when(() => validation.validate(field:  any(named: 'field'), value: any(named: 'value')))
+    .thenReturn('error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validateEmail(email);
+  });
+
 }
